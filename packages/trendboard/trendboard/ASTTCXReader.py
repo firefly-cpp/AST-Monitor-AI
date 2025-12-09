89bf08bf69f698af24037c1a34a4a660883a806b
Sebastian Pack <sebastian.pack@qc1.at>
2025-12-09 21:16:36 +0100

    update reader for dataframe and plot

--- packages/trendboard/trendboard/ASTTCXReader.py
+++ packages/trendboard/trendboard/ASTTCXReader.py
@@ -1,6 +1,10 @@
 from tcxreader.tcxreader import TCXReader, TCXExercise
 import os
 import numpy as np
+from collections import defaultdict
+from datetime import datetime
+import pandas as pd
+from dataclasses import asdict, is_dataclass
 
 class ASTTCXReader:
     def __init__(self, dir_path):
@@ -11,12 +15,35 @@ class ASTTCXReader:
     
     def __read_from_tcx_files(self):
         exercise_val = []
-        for file in os.listdir(self.dir_path):
+        # dir_length = len(os.listdir(self.dir_path))
+        for idx, file in enumerate(os.listdir(self.dir_path)):
             if file.endswith(".tcx"):
-                exercise_val.append(self.tcx_reader.read(os.path.join(self.dir_path, file), null_value_handling=1))
-        self.exercises_val = exercise_val    
-
+                exercise = self.tcx_reader.read(os.path.join(self.dir_path, file), null_value_handling=1)
+                exercise_val.append((exercise.start_time, exercise))
+            if idx >= 100:
+                self.exercises_val = exercise_val
+                return self.exercises_val                
+        self.exercises_val = exercise_val
+        return self.exercises_val
+    
     def get_calory_average(self) -> int:
-        calories = [e.calories for e in self.exercises_val]
+        calories = [e.calories for (d, e) in self.exercises_val]
         avg_calories = np.average(calories)
         return avg_calories
+    
+    def __exercises_to_df(self):
+        columns = ["activity_type", "start_time", "end_time", "calories", "avg_speed", "duration", "distance", "altitude_avg"]
+        df = pd.DataFrame([{f: getattr(e, f) for f in columns} for (d, e) in self.exercises_val])
+        return df
+
+    def get_exercise_dataframe(self):
+
+        df = self.__exercises_to_df()
+        df["start_time"] = pd.to_datetime(df["start_time"])
+        df["end_time"] = pd.to_datetime(df["end_time"])
+
+        df = df.dropna(subset=["start_time", "end_time"], how="all")
+        iso = df["start_time"].dt.isocalendar()        
+        df["iso_year_week"] = iso["year"].astype(str) + "" + iso["week"].astype(str).str.zfill(2)
+        print(df)
+        return df
--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,7 @@
 from trendboard.ASTTCXReader import ASTTCXReader
 import os
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 if __name__ == "__main__":
     # init tcx-reader
@@ -8,6 +10,24 @@ if __name__ == "__main__":
     reader = ASTTCXReader(directory_name)
 
     # calc average calories through multiple sessions
-    calories = reader.get_calory_average()
+    # calories = reader.get_calory_average()
+    # print(calories)
 
-    print(calories)
+    df = reader.get_exercise_dataframe()
+    df = df[df["activity_type"] == "Biking"]
+    df = df.sort_values("start_time")
+    df["avg_speed_per_avg_altitude"] = df["avg_speed"] / df["altitude_avg"]
+    #group sessions by week
+    
+    data_1 = df[["start_time", "avg_speed_per_avg_altitude"]]
+
+    fig, axes = plt.subplots(2, 1, figsize=(12, 9))
+    # Distance vs avg_speed (colored by start_time)
+    sc1 = axes[0].plot(        
+        data_1["start_time"],
+        data_1["avg_speed_per_avg_altitude"]        
+    )
+    axes[0].set_xlabel("start time")
+    axes[0].set_ylabel("avgerage speed to average altitude")
+    
+    plt.show()
